20da6ce74e6fe136422068d9a001af799154cbde
Christopher Friedt <chris@friedt.co>
2019-07-14 12:07:15 -0400

    Go through the cc26x0-cc13x0 target so that we can use BLEach
    Required updating updating two submodules with code from the latest TI SDK, along with a board.h with correct pinouts, some makefiles, etc.
    
    arch/cpu/cc26x0-cc13x0/lib/cc13xxware
    arch/cpu/simplelink-cc13xx-cc26xx/lib/coresdk_cc13xx_cc26xx
    
    updated board.h to get serial working

--- arch/cpu/cc26x0-cc13x0/ti-lib.h
+++ arch/cpu/cc26x0-cc13x0/ti-lib.h
@@ -200,11 +200,11 @@
 
 #if CPU_FAMILY_CC26X0R2
 #define ti_lib_chipinfo_chip_family_is_cc26xx(...)     ChipInfo_ChipFamilyIs_CC26x0(__VA_ARGS__)
-#define ti_lib_chipinfo_chip_family_is_cc13xx(...)     ChipInfo_ChipFamilyIs_CC13x0(__VA_ARGS__)
+#define ti_lib_chipinfo_chip_family_is_cc13xx(...)     (FAMILY_CC13x0 == ChipInfo_GetChipFamily()||ChipInfo_ChipFamilyIs_CC13x2_CC26x2(__VA_ARGS__))
 #define ti_lib_chipinfo_chip_family_is_cc26x0r2(...)   ChipInfo_ChipFamilyIs_CC26x0R2(__VA_ARGS__)
 #else
-#define ti_lib_chipinfo_chip_family_is_cc26xx(...)     ChipInfo_ChipFamilyIsCC26xx(__VA_ARGS__)
-#define ti_lib_chipinfo_chip_family_is_cc13xx(...)     ChipInfo_ChipFamilyIsCC13xx(__VA_ARGS__)
+#define ti_lib_chipinfo_chip_family_is_cc26xx(...)     (ChipInfo_ChipFamilyIsCC26x0(__VA_ARGS__)||ChipInfo_ChipFamilyIsCC26x0R2(__VA_ARGS__)||ChipInfo_ChipFamilyIsCC26x1(__VA_ARGS__)||ChipInfo_ChipFamilyIs_CC13x2_CC26x2(__VA_ARGS__))
+#define ti_lib_chipinfo_chip_family_is_cc13xx(...)     (FAMILY_CC13x0 == ChipInfo_GetChipFamily()||ChipInfo_ChipFamilyIs_CC13x2_CC26x2(__VA_ARGS__))
 #endif /* CPU_FAMILY_CC26X0R2 */
 
 #define ti_lib_chipinfo_get_hw_revision(...)           ChipInfo_GetHwRevision(__VA_ARGS__)
@@ -359,7 +359,11 @@
 
 #define ti_lib_prcm_inf_clock_configure_set(...)       PRCMInfClockConfigureSet(__VA_ARGS__)
 #define ti_lib_prcm_inf_clock_configure_get(...)       PRCMInfClockConfigureGet(__VA_ARGS__)
+#if 0
 #define ti_lib_prcm_mcu_power_off(...)                 PRCMMcuPowerOff(__VA_ARGS__)
+#else
+#define ti_lib_prcm_mcu_power_off(...)
+#endif
 #define ti_lib_prcm_mcu_power_off_cancel(...)          PRCMMcuPowerOffCancel(__VA_ARGS__)
 #define ti_lib_prcm_mcu_uldo_configure(...)            PRCMMcuUldoConfigure(__VA_ARGS__)
 #define ti_lib_prcm_audio_clock_enable(...)            PRCMAudioClockEnable(__VA_ARGS__)
@@ -397,8 +401,13 @@
 /* rfc.h */
 #include "driverlib/rfc.h"
 
+#if 0
 #define ti_lib_rfc_rtrim(...)                    RFCRTrim(__VA_ARGS__)
 #define ti_lib_rfc_adi3vco_ldo_voltage_mode(...) RFCAdi3VcoLdoVoltageMode(__VA_ARGS__)
+#else
+#define ti_lib_rfc_rtrim(...)
+#define ti_lib_rfc_adi3vco_ldo_voltage_mode(...)
+#endif
 #define ti_lib_rfc_hw_int_enable(...)            RFCHwIntEnable(__VA_ARGS__)
 #define ti_lib_rfc_hw_int_disable(...)           RFCHwIntDisable(__VA_ARGS__)
 #define ti_lib_rfc_hw_int_clear(...)             RFCHwIntClear(__VA_ARGS__)
